07c3e608924131ff98e1e07fda585a4b71c9b9a6
aynmedia <info@ayntech.in>
2024-10-28 18:39:30 +0530

    image

--- src/app/components/footer.jsx
+++ src/app/components/footer.jsx
@@ -9,7 +9,7 @@ const Footer = () => {
       <div className="container mx-auto grid grid-cols-1 md:grid-cols-3 gap-8 px-4">
         {/* Column 1 - About Us */}
         <div className="flex flex-col">
-          <Image
+          <img
             src="/images/GLCars.webp"
             alt="GLCars Logo"
             width={200}
